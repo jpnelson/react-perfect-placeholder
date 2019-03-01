5de1823f8bd3ef8c0fb25fe7132a07dc3bd3f580
Joshua Nelson <jonelson@atlassian.com>
2019-02-28 23:09:54 -0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import commonjs from 'rollup-plugin-commonjs';
+
+import pkg from './package.json';
+
+const name = 'PerfectPlaceholder';
+const path = 'dist/perfect-placeholder';
+const globals = {
+  'react-dom': 'ReactDOM',
+  react: 'React',
+};
+const external = Object.keys(globals);
+
+
+export default [
+  {
+    input: 'src/index.tsx',
+    output: {
+      file: pkg.module,
+      format: 'esm',
+    },
+    external,
+    plugins: [typescript()],
+  },
+
+  {
+    input: 'src/index.tsx',
+    output: {
+      name,
+      file: path + '.js',
+      format: 'umd',
+      globals,
+    },
+    external,
+    plugins: [typescript(), resolve(), commonjs()],
+  },
+
+  {
+    input: 'src/index.tsx',
+    output: {
+      name,
+      file: path + '.min.js',
+      format: 'umd',
+      globals,
+    },
+    external,
+    plugins: [typescript(), resolve(), commonjs(), terser()],
+  },
+];
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class PerfectPlaceholder extends React.Component {
+  public render() {
+    return this.props.children;
+  }
+}
